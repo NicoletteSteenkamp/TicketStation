b66765689290179eff4c4f7f17a98f397676f592
Nicolette <steenkampnicolette815@gmail.com>
2023-11-07 13:43:27 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -4,11 +4,10 @@ class AuthSystem {
     this.users = JSON.parse(localStorage.getItem("users")) || [];
   }
 
+  // Method to validate user login
   login(username, password) {
-    // Convert the provided username to lowercase (or uppercase)
-    const usernameLower = username.toLowerCase(); // Use .toLowerCase() to make it case-insensitive
+    const usernameLower = username.toLowerCase(); // Convert to lowercase for case-insensitive comparison
 
-    // Check if a user with the provided username (case-insensitive) and password exists in the users array
     const user = this.users.find(
       (user) =>
         user.username.toLowerCase() === usernameLower &&
@@ -18,7 +17,64 @@ class AuthSystem {
     if (user) {
       return `Login successful. Welcome, ${user.username}!`;
     } else {
-      return "Invalid username or password. Please try again, or sign-up";
+      return "Invalid username or password. Please try again, or sign up.";
     }
   }
 }
+
+class LoginForm {
+  constructor(authSystem) {
+    this.authSystem = authSystem;
+
+    // Selecting form elements
+    this.form = document.querySelector("form");
+    this.nameInput = document.querySelector('input[type="text"]');
+    this.passwordInput = document.querySelector('input[type="password"]');
+    this.rememberCheckbox = document.querySelector('input[type="checkbox"]');
+    this.button = document.querySelector(".con-button-style"); // Updated button class name
+
+    // Registering the submitForm method as the form's submit event handler
+    this.form.addEventListener("submit", this.submitForm.bind(this));
+  }
+
+  // Method to validate the form inputs
+  validateForm() {
+    const username = this.nameInput.value.trim();
+    const password = this.passwordInput.value.trim();
+
+    if (!username || !password) {
+      throw new Error("Please fill in both username and password fields.");
+    }
+  }
+
+  // Method to handle form submission
+  submitForm(event) {
+    event.preventDefault(); // Prevent form submission to avoid page refresh
+
+    try {
+      this.validateForm();
+      const username = this.nameInput.value.trim();
+      const password = this.passwordInput.value.trim();
+      const result = this.authSystem.login(username, password);
+      alert(result);
+      this.clearForm();
+      // Redirect to the "index.html" page
+      window.location.href = "index.html";
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  // Method to clear the form inputs
+  clearForm() {
+    this.nameInput.value = "";
+    this.passwordInput.value = "";
+    this.rememberCheckbox.checked = false;
+  }
+}
+
+// Create an instance of the AuthSystem class
+const authSystem = new AuthSystem();
+
+// Create an instance of the LoginForm class and pass the authSystem instance
+const loginForm = new LoginForm(authSystem);
